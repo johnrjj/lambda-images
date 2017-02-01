4a036e71c893ef63ed205de5a4da4d418f7d6f01
John Johnson <johnrjj@gmail.com>
2017-01-31 21:24:42 -0500

    Removing comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,5 +10,4 @@ const App = () => (
   </Router>
 );
 
-
 export default App;
--- frontend/src/containers/GalleryPage.js
+++ frontend/src/containers/GalleryPage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Radium from 'radium';
-import { withRouter } from 'react-router-dom';
 
 const GalleryPage = withRouter(({push}) => {
-  // messing with programatic redirects...
-  setTimeout(() => push('/'), 1000);
   return (
     <div>
       <h1>a gallery page</h1>
