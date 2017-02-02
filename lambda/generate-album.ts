61dcaacb5343debbc6fa2a5b4b4463b341d35862
John Johnson <johnrjj@gmail.com>
2017-02-01 23:23:32 -0500

    Adding in sample generate album code

---
+++ lambda/generate-album.ts
@@ -0,0 +1,102 @@
+import * as AWS from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+import { extension } from 'mime';
+import { Context, Callback } from 'aws-lambda';
+
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+});
+
+const s3 = new AWS.S3();
+
+const generateUniqueKey = (): string => {
+  const id: string = uuid();
+  return id;
+}
+
+const generateS3PresignedUrl = (actionKey: string, parameters): Promise<string> =>
+  new Promise((resolve, reject) => {
+    s3.getSignedUrl(actionKey, parameters, (err, url) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(url)
+      }
+    })
+  });
+
+const getSignedUrl = async (event: any, context: Context, callback: Callback) => {
+  console.log('Dumping event', event);
+  console.log('Dumping context', context);
+  let fileType = null;
+  if (event.headers['Content-Type']) {
+    const mimeType = event.headers['Content-Type'];
+    fileType = extension(mimeType);
+    console.log(`Detected file type: ${fileType}`);
+  } else {
+    fileType = 'png';
+  }
+  try {
+    const url = await generateS3PresignedUrl('putObject', {
+      Bucket: process.env.BUCKET,
+      Key: `images/${generateUniqueKey()}.${fileType}`,
+      ContentType: 'application/octet-stream',
+      ACL: 'public-read',
+    });
+    const response = {
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      },
+      statusCode: 200,
+      body: JSON.stringify({ url }),
+    };
+    return callback(null, response);
+  } catch (err) {
+    return callback(err);
+  }
+};
+
+const generateAlbum = async (event: any, context: Context, callback: Callback) => {
+
+  const { body } = event;
+
+  if (!body) {
+    callback(new Error("Body null in generateAlbum POST request"));
+  }
+
+  try {
+    const images = JSON.parse(body);
+
+    const imagesWithPresignedUrls = Promise.all(images.map(image => {
+      const fileType = extension(image.mimeType);
+      return generateS3PresignedUrl('putObject', {
+        Bucket: process.env.BUCKET,
+        Key: `images/${generateUniqueKey()}.${fileType}`,
+        ContentType: 'application/octet-stream',
+        ACL: 'public-read',
+      });
+    }));
+
+    const album = {
+      url: generateUniqueKey(),
+      images: imagesWithPresignedUrls,
+    };
+
+    const response = {
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      },
+      statusCode: 200,
+      body: JSON.stringify({ event, context, album }),
+    };
+    
+    return callback(null, response);
+  } catch (err) {
+    callback(err);
+  }
+}
+
+export {
+  generateAlbum,
+};
--- lambda/handler.ts
+++ lambda/handler.ts
@@ -2,7 +2,7 @@ import { auth } from './auth';
 import { privateEndpoint, publicEndpoint } from './hello-world';
 import { getSignedUrl } from './sign-url';
 import { createThumbnail } from './create-thumbnail';
-
+import { generateAlbum } from './generate-album';
 
 export {
   privateEndpoint,
@@ -10,5 +10,6 @@ export {
   auth,
   getSignedUrl,
   createThumbnail,
+  generateAlbum,
 };
 
