16270ebd906f1908492cdfb4ce8c4ce00ecd07d4
John Johnson <johnrjj@gmail.com>
2017-02-03 12:38:43 -0500

    Removing generated files

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var React = require("react");
-var ReactDOM = require("react-dom");
-var App_1 = require("./App");
-require("./index.css");
-ReactDOM.render(<App_1.default />, document.getElementById('root'));
