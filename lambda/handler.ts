9254f969e8d9f15a039c54b513296ebfcb2113b4
John Johnson <johnrjj@gmail.com>
2017-02-26 19:58:08 -0500

    Removing unused services

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,5 +1,4 @@
 import { auth } from './auth';
-import { privateEndpoint, publicEndpoint } from './hello-world';
 import { createThumbnail } from './create-thumbnail';
 import { generateAlbum } from './generate-album';
 import { updateDescription } from './update-file-description';
@@ -7,8 +6,6 @@ import { checkCollectionStatus } from './upload-progress';
 import { getCollectionContents } from './get-collection-contents';
 
 export {
-  privateEndpoint,
-  publicEndpoint,
   auth,
   createThumbnail,
   generateAlbum,
