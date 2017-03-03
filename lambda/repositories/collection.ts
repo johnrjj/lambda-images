ddd18b94f9688b024c42dc315200b2bfd4d6cf5b
John Johnson <johnrjj@gmail.com>
2017-03-02 21:56:30 -0500

    ddd

---
+++ lambda/repositories/collection.ts
@@ -0,0 +1,60 @@
+import * as AWS from 'aws-sdk';
+
+const COLLECTION_DDB_TABLE = process.env.COLLECTION_TABLE;
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const getCollectionContents = (collectionId: string, tableName: string = COLLECTION_DDB_TABLE): Promise<Array<string>> => {
+  return new Promise((accept, reject) => {
+    const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+      TableName: tableName,
+      KeyConditionExpression: '#id = :id',
+      ExpressionAttributeNames: {
+        '#id': 'id',
+      },
+      ExpressionAttributeValues: {
+        ":id": collectionId,
+      }
+    };
+
+    docClient.query(params, (err, data) => {
+      if (err) {
+        console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(item => console.log(item));
+        const entries: Array<string> = data.Items[0].entries
+        accept(entries);
+      }
+    });
+  });
+};
+
+const createCollection = (album: object) => {
+  return new Promise((accept, reject) => {
+    const params = {
+      TableName: COLLECTION_DDB_TABLE,
+      Item: album,
+    };
+    return docClient.put(params, function (err, data) {
+      if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+        accept(data)
+      }
+    });
+  })
+};
+
+
+const createClient = (tableName: string) => ({
+  getCollectionContents
+});
+
+export {
+  getCollectionContents,
+  createCollection,
+}
---
+++ lambda/repositories/file.ts
@@ -0,0 +1,135 @@
+import * as AWS from 'aws-sdk';
+
+const FILE_DDB_TABLE = process.env.DYNAMO;
+
+const dynamodb = new AWS.DynamoDB();
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const getFileData = (fileId) => {
+  return new Promise((accept, reject) => {
+    const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+      TableName: FILE_DDB_TABLE,
+      KeyConditionExpression: "#id = :id",
+      ExpressionAttributeNames: {
+        "#id": "id",
+      },
+      ExpressionAttributeValues: {
+        ":id": fileId,
+      }
+    };
+    docClient.query(params, (err, data) => {
+      if (err) {
+        console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(item => console.log(item));
+        accept(data);
+      }
+    });
+  });
+}
+
+const updateMetadata = (id: string, s3Key: string): Promise<AWS.DynamoDB.UpdateItemOutput> => {
+  return new Promise((accept, reject) => {
+    const updateDescriptionParams: AWS.DynamoDB.UpdateItemInput = {
+      TableName: FILE_DDB_TABLE,
+      Key: {
+        id,
+      },
+      UpdateExpression: "set s3key = :d",
+      ExpressionAttributeValues: {
+        ":d": s3Key,
+      },
+      ReturnValues: "UPDATED_NEW"
+    };
+    docClient.update(updateDescriptionParams, (err, data) => {
+      if (err) {
+        console.log('err putting', err);
+        reject(err);
+      } else {
+        console.log('succesfullyput item', data);
+        accept(data);
+      }
+    });
+  });
+}
+
+const updateDatabaseWithThumbnailKey = (fileId: string, thumbnailS3Key: string): Promise<AWS.DynamoDB.UpdateItemOutput> => {
+  return new Promise((accept, reject) => {
+    const updateDescriptionParams: AWS.DynamoDB.UpdateItemInput = {
+      TableName: FILE_DDB_TABLE,
+      Key: {
+        id: fileId,
+      },
+      UpdateExpression: "set thumbnails3key = :d",
+      ExpressionAttributeValues: {
+        ":d": thumbnailS3Key,
+      },
+      ReturnValues: "UPDATED_NEW"
+    };
+    docClient.update(updateDescriptionParams, (err, data) => {
+      if (err) {
+        console.log('err putting', err);
+        reject(err);
+      } else {
+        console.log('succesfullyput item', data);
+        accept(data);
+      }
+    });
+  });
+}
+
+
+
+const updateDatabaseWithMediaMetadata = (id: string, height: number, width: number): Promise<AWS.DynamoDB.UpdateItemOutput> => {
+  return new Promise((accept, reject) => {
+    const updateDescriptionParams: AWS.DynamoDB.UpdateItemInput = {
+      TableName: FILE_DDB_TABLE,
+      Key: { id },
+      UpdateExpression: 'set height = :h, width = :w',
+      ExpressionAttributeValues: {
+        ':h': height,
+        ':w': width,
+      },
+      ReturnValues: "UPDATED_NEW"
+    };
+    docClient.update(updateDescriptionParams, (err, data) => {
+      return (err) 
+        ? console.log('err putting', err) || reject(err)
+        : console.log('succesfullyput item', data) || accept(data);
+    });
+  });
+}
+
+
+const createFileMetadata = (id: string) => {
+  return new Promise((accept, reject) => {
+    let params = {
+      TableName: FILE_DDB_TABLE,
+      Item: {
+        id: { S: id },
+        timestamp: { S: (new Date().toJSON().toString()) }
+      }
+    };
+    dynamodb.putItem(params, (err, data) => {
+      if (err) {
+        console.log('error putting item', err);
+        reject(err);
+      } else {
+        console.log('succesfullyput item', data);
+
+        accept(data);
+      }
+    });
+  });
+};
+
+export {
+  getFileData,
+  createFileMetadata,
+  updateMetadata,
+  updateDatabaseWithMediaMetadata,
+  updateDatabaseWithThumbnailKey,
+}
+
